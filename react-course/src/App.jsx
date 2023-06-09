f6a4bae70e8b165a36a3903f541ea543a0c37a41
Fernando Cigoy <fernandoc32667@gmail.com>
2023-06-08 22:27:48 -0300

    Preparacion de entorno para iniciar proyecto

---
+++ react-course/src/App.jsx
@@ -0,0 +1,13 @@
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='bg-gray-700'>
+      Hola mundo react
+    </div>
+  )
+}
+
+export default App
